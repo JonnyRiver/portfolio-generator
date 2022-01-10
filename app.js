8af409d046ed7293d4285268e53ed91664649465
JonnyRiver <jonnyriver6@gmail.com>
2022-01-10 14:02:40 -0500

    add page-template.js

--- app.js
+++ app.js
@@ -1,22 +1,13 @@
-// var commandLineArgs = process.argv;
-// console.log(commandLineArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-// var profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArgs)
+const profileDataArgs = process.argv.slice(2);
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
-
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-    console.log(profileDataArr[i]);
-    }
-};
-
-console.log('============');
-
-profileDataArgs.forEach((profileItem => console.log(profileItem)
-));
+const [name, github] = profileDataArgs;
 
 
-printProfileData(profileDataArgs);
+fs.writeFile('./index.html', generatePage(name,github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!')
+});
